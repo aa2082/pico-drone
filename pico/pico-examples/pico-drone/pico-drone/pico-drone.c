1c986757fbb1ee6e823b3a8925a92a585048aa64
aa2082 <aa2082@cam.ac.uk>
2022-01-27 20:33:06 +0000

    motor spinning

--- pico/pico-examples/pico-drone/pico-drone/pico-drone.c
+++ pico/pico-examples/pico-drone/pico-drone/pico-drone.c
@@ -19,6 +19,46 @@ static void mpu6050_reset() {
     i2c_write_blocking(i2c_default, addr, buf, 2, false);
 }
 
+static void TMC6300_setup(uint uh,uint ul,uint vh,uint vl,uint wh,uint wl) {
+    gpio_init(uh);
+    gpio_init(ul);
+    gpio_init(vh);
+    gpio_init(vl);
+    gpio_init(wh);
+    gpio_init(wl);
+
+    gpio_set_dir(uh, GPIO_OUT);
+    gpio_set_dir(ul, GPIO_OUT);
+    gpio_set_dir(vh, GPIO_OUT);
+    gpio_set_dir(vl, GPIO_OUT);
+    gpio_set_dir(wh, GPIO_OUT);
+    gpio_set_dir(wl, GPIO_OUT);
+}
+static void TMC6300_run(uint uh,uint ul,uint vh,uint vl,uint wh,uint wl) {
+    gpio_put(uh,1); 
+    gpio_put(vl,1);
+    sleep_ms(1);
+
+    gpio_put(vl,0);
+    gpio_put(wl,1);
+    sleep_ms(1);
+
+    gpio_put(uh,0);
+    gpio_put(vh,1);
+    sleep_ms(1);
+
+    gpio_put(wl,0);
+    gpio_put(ul,1);
+    sleep_ms(1);
+
+    gpio_put(vh,0);
+    gpio_put(wh,1);
+    sleep_ms(1);
+
+    gpio_put(ul,0);
+    gpio_put(vl,1);
+    sleep_ms(1);
+}
 static void mpu6050_read_gyro(int16_t gyro[3]) {
     // For this particular device, we send the device the register we want to read
     // first, then subsequently read from the device. The register is auto incrementing
@@ -39,7 +79,10 @@ static void mpu6050_read_gyro(int16_t gyro[3]) {
 }
 
 int main() {
-    stdio_init_all();
+    
+    //stdio_init_all();
+    
+    
     /*
     while (true) {
         printf("TEST2!\n");
@@ -48,6 +91,7 @@ int main() {
     return 0;
     */
     // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    /*
     i2c_init(i2c_default, 400 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
@@ -59,15 +103,44 @@ int main() {
     mpu6050_reset();
 
     int16_t gyro[3];
-
+                 //uh,ul,vh,vl,wh,wl
+     
+    */
+    TMC6300_setup(10,11,12,13,14,15);
     while (1) {
-        mpu6050_read_gyro(gyro);
+        
+        //mpu6050_read_gyro(gyro);
 
         // These are the raw numbers from the chip, so will need tweaking to be really useful.
         // See the datasheet for more information
-        printf("%d,%d,%d\n", gyro[0], gyro[1], gyro[2]);
+        //printf("%d,%d,%d\n", gyro[0], gyro[1], gyro[2]);
 
-        sleep_ms(0.0025);
+        //sleep_ms(0.125);
+        
+        //TMC6300_run(10,11,12,13,14,15);
+
+        gpio_put(10,1); 
+        gpio_put(13,1);
+        sleep_ms(1);
+
+        gpio_put(13,0);
+        gpio_put(15,1);
+        sleep_ms(1);
+
+        gpio_put(10,0);
+        gpio_put(12,1);
+        sleep_ms(1);
+
+        gpio_put(15,0);
+        gpio_put(11,1);
+        sleep_ms(1);
+
+        gpio_put(12,0);
+        gpio_put(14,1);
+        sleep_ms(1);
+
+        gpio_put(11,0);
+        gpio_put(13,1);
+        sleep_ms(1);
     }
-
 }
