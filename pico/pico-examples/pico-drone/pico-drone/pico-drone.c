3f105a4ba54810e2b2c57627f506ef946fbf58d1
aa2082 <aa2082@cam.ac.uk>
2022-01-27 19:05:48 +0000

    400k sampling

--- pico/pico-examples/pico-drone/pico-drone/pico-drone.c
+++ pico/pico-examples/pico-drone/pico-drone/pico-drone.c
@@ -19,25 +19,16 @@ static void mpu6050_reset() {
     i2c_write_blocking(i2c_default, addr, buf, 2, false);
 }
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+static void mpu6050_read_gyro(int16_t gyro[3]) {
     // For this particular device, we send the device the register we want to read
     // first, then subsequently read from the device. The register is auto incrementing
     // so we don't need to keep sending the register we want, just the first.
 
     uint8_t buffer[6];
 
-    // Start reading acceleration registers from register 0x3B for 6 bytes
-    uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
-    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
-
-    for (int i = 0; i < 3; i++) {
-        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
-    }
-
     // Now gyro data from reg 0x43 for 6 bytes
     // The register is auto incrementing on each read
-    val = 0x43;
+    uint8_t val = 0x43;
     i2c_write_blocking(i2c_default, addr, &val, 1, true);
     i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
 
@@ -45,13 +36,6 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
         gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
     }
 
-    // Now temperature from reg 0x41 for 2 bytes
-    // The register is auto incrementing on each read
-    val = 0x41;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 2, false);  // False - finished with bus
-
-    *temp = buffer[0] << 8 | buffer[1];
 }
 
 int main() {
@@ -74,20 +58,16 @@ int main() {
 
     mpu6050_reset();
 
-    int16_t acceleration[3], gyro[3], temp;
+    int16_t gyro[3];
 
     while (1) {
-        mpu6050_read_raw(acceleration, gyro, &temp);
+        mpu6050_read_gyro(gyro);
 
         // These are the raw numbers from the chip, so will need tweaking to be really useful.
         // See the datasheet for more information
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        // Temperature is simple so use the datasheet calculation to get deg C.
-        // Note this is chip temperature.
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        printf("%d,%d,%d\n", gyro[0], gyro[1], gyro[2]);
 
-        sleep_ms(1000);
+        sleep_ms(0.0025);
     }
 
 }
